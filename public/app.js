2dab685d4b18abace97564962f72314c76addf4e
Linh-Vu093 <ai.vu557@education.nsw.gov.au>
2025-08-04 20:34:23 +1000

    Added cryptography and data storage

--- public/app.js
+++ public/app.js
@@ -1,4 +1,5 @@
 // const SURVEY_ID = 1;
+import * as CryptoJS from 'crypto-js';
 
 const survey = new Survey.Model(surveyJson);
 survey.onComplete.add((sender) => {
@@ -51,6 +52,50 @@ survey.onTextMarkdown.add((_, options) => {
 // Indicate correct and incorrect answers at startup
 survey.getAllQuestions().forEach(question => changeTitle(question));
 survey.render(document.getElementById("surveyContainer"));
+
+import * as CryptoJS from 'crypto-js';
+
+function storageEncryption() {
+    /**
+  * secret key should be stored in a safe place. This is only for a demo purpose.
+  */
+    let _key = "secret_key"
+
+    function encrypt(txt) {
+        return CryptoJS.AES.encrypt(txt, _key).toString();
+    }
+
+    function decrypt(txtToDecrypt) {
+        return CryptoJS.AES.decrypt(txtToDecrypt, _key).toString(CryptoJS.enc.Utf8);
+    }
+
+    function manipulateLocalStorage() {
+        Storage.prototype.setEncryptedItem = (key, value) => {
+            localStorage.setItem(key, encrypt(value));
+        };
+
+        Storage.prototype.getDecryptedItem = (key) => {
+            let data = localStorage.getItem(key) || "";
+            return decrypt(data) || null;
+        }
+    }
+ /**
+  * First call this function to add our custom functions to the Storage interface
+  * 
+  */
+    manipulateLocalStorage();
+    /**
+     * you can use the setEncryptedItem and getDecryptedItem functions
+     * to encrypt and decrypt the data
+     * */ 
+
+    localStorage.setEncryptedItem("token", "12345");
+    const token = localStorage.getDecryptedItem("token");
+    console.log(token);
+}
+storageEncryption();
+
+
 
 // function saveSurveyResults(url, json) {
 //     fetch(url, {
